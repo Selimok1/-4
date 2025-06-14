f915e0f1ae089f73eacf844292c8ec0788c887c9
Selim <ypselimok29@gmail.com>
2025-06-14 14:00:56 +0300

    4 main.py

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ class Book:
 
     def info(self):
         return f"Название: {self.title}, Автор: {self.author}, Год: {self.year}"
+class EBook(Book):
+    def info(self):
+        return f"EBook: {self.title}, {self.author}, {self.year}"
 
-my_book = Book("1984", "Джордж Видер", 1939)
+my_book = Book("1984", "Джордж Оруэлл", 1949)
+my_ebook = EBook("1984", "Джордж Оруэлл", 1949)
+
 print(my_book.info())
+print(my_ebook.info())
